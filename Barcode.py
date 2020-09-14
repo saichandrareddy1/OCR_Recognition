e8502f45126b6a34735bf1cc93bdc2712286d08f
saichandrareddy1 <vsaichandrareddy@gmail.com>
2020-09-14 15:53:05 +0530

    update

--- Barcode.py
+++ Barcode.py
@@ -1,62 +1,72 @@
-
-
-# USAGE
-# python detect_barcode.py --image images/barcode_01.jpg
-
 # import the necessary packages
 import numpy as np
 import argparse
 import imutils
 import cv2
+from pyzbar.pyzbar import decode
+from PIL import Image
 
-# construct the argument parse and parse the arguments
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-i", "--image", required = True,
-# 	help = "path to the image file")
-# args = vars(ap.parse_args())
+def Barcode_Scanner(path_to_image):
+	# load the image and convert it to grayscale
+	image = cv2.imread(path_to_image)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# load the image and convert it to grayscale
-#image = cv2.imread(args["image"])
-image = cv2.imread("/home/saireddy/OCR/OCR_Recognition/package_images/3/03.jpeg")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	# compute the Scharr gradient magnitude representation of the images
+	# in both the x and y direction using OpenCV 2.4
+	ddepth = cv2.CV_32F if imutils.is_cv2() else cv2.CV_32F
+	gradX = cv2.Sobel(gray, ddepth=ddepth, dx=1, dy=0, ksize=-1)
+	gradY = cv2.Sobel(gray, ddepth=ddepth, dx=0, dy=1, ksize=-1)
 
-# compute the Scharr gradient magnitude representation of the images
-# in both the x and y direction using OpenCV 2.4
-ddepth = cv2.CV_32F if imutils.is_cv2() else cv2.CV_32F
-gradX = cv2.Sobel(gray, ddepth=ddepth, dx=1, dy=0, ksize=-1)
-gradY = cv2.Sobel(gray, ddepth=ddepth, dx=0, dy=1, ksize=-1)
+	# subtract the y-gradient from the x-gradient
+	gradient = cv2.subtract(gradX, gradY)
+	gradient = cv2.convertScaleAbs(gradient)
 
-# subtract the y-gradient from the x-gradient
-gradient = cv2.subtract(gradX, gradY)
-gradient = cv2.convertScaleAbs(gradient)
+	# blur and threshold the image
+	blurred = cv2.blur(gradient, (9, 9))
+	(_, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
 
-# blur and threshold the image
-blurred = cv2.blur(gradient, (9, 9))
-(_, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
+	# construct a closing kernel and apply it to the thresholded image
+	kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
+	closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 
-# construct a closing kernel and apply it to the thresholded image
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
-closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+	# perform a series of erosions and dilations
+	closed = cv2.erode(closed, None, iterations = 1)
+	closed = cv2.dilate(closed, None, iterations = 1)
 
-# perform a series of erosions and dilations
-closed = cv2.erode(closed, None, iterations = 1)
-closed = cv2.dilate(closed, None, iterations = 1)
+	# find the contours in the thresholded image, then sort the contours
+	# by their area, keeping only the largest one
+	cnts = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+	c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
 
-# find the contours in the thresholded image, then sort the contours
-# by their area, keeping only the largest one
-cnts = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
-c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+	# compute the rotated bounding box of the largest contour
+	rect = cv2.minAreaRect(c)
+	box = cv2.cv.BoxPoints(rect) if imutils.is_cv2() else cv2.boxPoints(rect)
+	box = np.int0(box)
 
-# compute the rotated bounding box of the largest contour
-rect = cv2.minAreaRect(c)
-box = cv2.cv.BoxPoints(rect) if imutils.is_cv2() else cv2.boxPoints(rect)
-box = np.int0(box)
+	# draw a bounding box arounded the detected barcode and display the
+	min_y = int(np.min(box[:,-1]))
+	max_y = int(np.max(box[:,-1]))
+	min_x = int(np.min(box[:,0]))
+	max_x = int(np.max(box[:,0]))
+	crop_ima = image[min_y:max_y, min_x:max_x]
 
-# draw a bounding box arounded the detected barcode and display the
-# image
-cv2.drawContours(image, [box], -1, (0, 255, 0), 3)
-cv2.imshow("Image", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+	# save cropped image
+	#cv2.imwrite("cropped.jpg", crop_ima)
+	sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+	sharpen = cv2.filter2D(crop_ima, -1, sharpen_kernel)
+	col = cv2.cvtColor(sharpen, cv2.COLOR_RGB2GRAY)
+	thres, im_bw = cv2.threshold(col, 127, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+	gray = cv2.bitwise_not(im_bw)
+	kernel = np.ones((2, 1), np.uint8)
+	img = cv2.erode(gray, kernel, iterations=2)
+	img = cv2.dilate(img, kernel, iterations=2)
+	print("decoding", decode(img))
+	# draw a bounding box arounded the detected barcode and display the
+	# image
+	cv2.drawContours(image, [box], -1, (0, 255, 0), 3)
+	#cv2.imshow("Image", im_bw)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+	return crop_ima, decode(img)
--- OCR_finder.py
+++ OCR_finder.py
@@ -2,9 +2,17 @@ import cv2
 from pytesseract import pytesseract
 from PIL import Image
 import numpy as np
-
+import os
+from Barcode import Barcode_Scanner
 
 # preprocessing
+def filesaver(filname, text):
+    f = open(filname, "w+")
+
+    for i in range(len(text)):
+        f.write(f"{text[i]}")
+    f.close()
+
 # gray scale
 def gray(img):
     
@@ -21,102 +29,52 @@ def blur(img) :
 
 
 # threshold
-def threshold(img):
+def threshold(dir_, file_, total):
+    print(total, dir_, file_)
     #pixels with value below 100 are turned black (0) and those with higher value are turned white (255)
-    images = []
-    techs = [cv2.THRESH_BINARY,
-        cv2.THRESH_BINARY_INV,
-        cv2.THRESH_TRUNC,
-        cv2.THRESH_TOZERO,
-        cv2.THRESH_TOZERO_INV]
+    for i in range(len(file_)):
+        print(i)
+        print(file_[i])
+        img = cv2.imread(os.path.join(dir_, file_[i]), 0)
+        #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)[1]    
+        gray = cv2.bitwise_not(img)
+        kernel = np.ones((2, 1), np.uint8)
+        img = cv2.erode(gray, kernel, iterations=1)
+        img = cv2.dilate(img, kernel, iterations=1)
+        #print(os.path.exists(os.path.join(dir_+f"/{file_}"+"thres", file_)), os.path.join(dir_+f"/{file_}"+"thres", file_))
+        if os.path.exists(os.path.join(dir_+"/", file_[i]+"thres")):
+            pass
+        else:
+            path = os.path.join(dir_+"/", file_[i]+"thres")
+            os.mkdir(path)
+            cv2.imwrite(path+f"/{file_[i]}+thres.png", img)
+            out_below = pytesseract.image_to_string(img)
+            print("out_below",  out_below)
+            filesaver(path+f"/{file_[i]}+thres.txt", out_below)
+            print(path)
+            cropimage, textfile = Barcode_Scanner(path_to_image=os.path.join(dir_, file_[i]))
+            cv2.imwrite(path+f"/{file_[i]}+cropimg.png", cropimage)
+            filesaver(path+f"/{file_[i]}+decode.txt", textfile)
 
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    count = 0
-    for i in range(len(techs)):
-         for j in range(len(techs)):
-               img = cv2.threshold(img, 128, 255, techs[i] & techs[j])[1]    
-               gray = cv2.bitwise_not(img)
-               kernel = np.ones((2, 1), np.uint8)
-               img = cv2.erode(gray, kernel, iterations=2)
-               img = cv2.dilate(img, kernel, iterations=2)
-               images.append(img)
-               cv2.imwrite(f"images/img_threshold{count}.png",img)
-               count +=1 
-    return images
 
-def new():
-    img = cv2.imread('/home/saireddy/OCR/OCR_Recognition/package_images/1/2.jpeg')
-    #Alternatively: can be skipped if you have a Blackwhite image
-    if img is not None:
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    else:
-        print("ss")
-    gray, img_bin = cv2.threshold(gray,0,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    gray = cv2.bitwise_not(img_bin)
-    kernel = np.ones((2, 1), np.uint8)
-    img = cv2.erode(gray, kernel, iterations=1)
-    img = cv2.dilate(img, kernel, iterations=1)
-    out_below = pytesseract.image_to_string(img)
-    print("OUTPUT:", out_below)
+def collectimages():
+    dirname_li = []
+    filenames_li = []
+    total_path = []
+    for dirname, _, filenames in os.walk(os.getcwd()+'/package_images'):
+        for filename in filenames:
+            if os.path.join(dirname, filename).endswith('.jpeg') or os.path.join(dirname, filename).endswith('.jpg'):
+                dirname_li.append(dirname)
+                filenames_li.append(filenames)
+                total_path.append(os.path.join(dirname, filename))
+            else:
+                pass
+    
+    return dirname_li, filenames_li, total_path
 
-new()
 
-def main():
-     # read image
-     im = cv2.imread('/home/saireddy/OCR/OCR_Recognition/package_images/1/2.jpeg')
-     #lis = [gray(im), blur(im)]
-     li = threshold(im)
-     print(li)
-     # configurations
-     config = ('-l eng --oem 1 --psm 3')
-     # pytessercat
-     for i in li:
-          #out_below = pytesseract.image_to_string(img)
-          text = pytesseract.image_to_string(i, config=config)
-          # print text
-          
-          text = text.split('\n')
-          if len(text) > 4:
-            print(len(text), text)
-          else:
-              pass
-
-main()
-
-# def main():
-#         #pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\\tesseract.exe'
-#         # Get File Name from Command Line
-#         path = '/home/saireddy/OCR/OCR_Recognition/package_images/1/01.jpeg'
-#         # load the image
-    
-#         image = cv2.imread(path)
-#         # Convert image to grayscale
-    
-#         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-#         temp = input("Do you want to pre-process the image ?nThreshold : 1nGrey : 2nNone : 0nEnter your choice : ").strip()
-    
-#          # If user enter 1, Process Threshold
-#         if temp == "1":
-#              #gray = cv2.medianBlur(gray, 3)
-#              gray = cv2.threshold(gray, 0, 255, cv2.THRESH_TOZERO + cv2.THRESH_OTSU)[1]
-#         elif temp == "2":
-#              gray = cv2.medianBlur(gray, 3)
-    
-#         # store grayscale image as a temp file to apply OCR
-    
-#         filename = "{}.png".format("temp")
-        
-#         cv2.imwrite(filename, gray)
-    
-#         # load the image as a PIL/Pillow image, apply OCR, and then delete the temporary file
-    
-#         text = pytesseract.image_to_string(Image.open(filename))
-    
-#         print(text)
-
-# try:
-#      main()
-# except Exception as e:
-#      print(e.args) 
-#      print(e.__cause__)
+if __name__ == "__main__":
+    dir_, file_, total = collectimages()
+    for i in range(len(dir_)):
+        threshold(dir_[i], file_[i], total[i])
