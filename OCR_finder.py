7a688622152ec4ea37cebe31b3701c8c46f69ddf
saichandrareddy1 <vsaichandrareddy@gmail.com>
2020-09-14 23:41:31 +0530

    Time Complexity Update

--- OCR_finder.py
+++ OCR_finder.py
@@ -28,52 +28,67 @@ def blur(img) :
 
 
 # threshold
-def threshold(dir_, file_, total):
-    print(total, dir_, file_)
+def threshold(dir_, file_):
+    print(dir_, file_)
     #pixels with value below 100 are turned black (0) and those with higher value are turned white (255)
     for i in range(len(file_)):
-        print(i)
-        print(file_[i])
+        
         img = cv2.imread(os.path.join(dir_, file_[i]), 0)
+        print(os.path.join(dir_, file_[i]))
         #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)[1]    
+        img = cv2.threshold(img, 0, 255, cv2.THRESH_TOZERO | cv2.THRESH_TOZERO)[1]    
         gray = cv2.bitwise_not(img)
         kernel = np.ones((2, 1), np.uint8)
-        img = cv2.erode(gray, kernel, iterations=1)
-        img = cv2.dilate(img, kernel, iterations=1)
-        #print(os.path.exists(os.path.join(dir_+f"/{file_}"+"thres", file_)), os.path.join(dir_+f"/{file_}"+"thres", file_))
+        img = cv2.erode(gray, kernel, iterations=2)
+        img = cv2.dilate(img, kernel, iterations=2)
+        #print(gray)
+        #print(len([i for i in (os.listdir(os.path.join(dir_+"/", file_[i]+"thres"))) ]))
+        print(dir_+f"/{file_[i]}"+"thres")
         if os.path.exists(os.path.join(dir_+"/", file_[i]+"thres")):
-            pass
-        else:
+
             path = os.path.join(dir_+"/", file_[i]+"thres")
-            os.mkdir(path)
             cv2.imwrite(path+f"/{file_[i]}+thres.png", img)
             out_below = pytesseract.image_to_string(img)
-            print("out_below",  out_below)
+            #print("out_below",  out_below)
             filesaver(path+f"/{file_[i]}+thres.txt", out_below)
-            print(path)
+            #print(path)
             cropimage, textfile = Barcode_Scanner(path_to_image=os.path.join(dir_, file_[i]))
             cv2.imwrite(path+f"/{file_[i]}+cropimg.png", cropimage)
             filesaver(path+f"/{file_[i]}+decode.txt", textfile)
 
+        else:
+            path = os.path.join(dir_+"/", file_[i]+"thres")
+            os.mkdir(path)
+            path = os.path.join(dir_+"/", file_[i]+"thres")
+            cv2.imwrite(path+f"/{file_[i]}+thres.png", img)
+            out_below = pytesseract.image_to_string(img)
+            #print("out_below",  out_below)
+            filesaver(path+f"/{file_[i]}+thres.txt", out_below)
+            #print(path)
+            cropimage, textfile = Barcode_Scanner(path_to_image=os.path.join(dir_, file_[i]))
+            cv2.imwrite(path+f"/{file_[i]}+cropimg.png", cropimage)
+            filesaver(path+f"/{file_[i]}+decode.txt", textfile)
+
+            
 
 def collectimages():
     dirname_li = []
     filenames_li = []
-    total_path = []
     for dirname, _, filenames in os.walk(os.getcwd()+'/package_images'):
-        for filename in filenames:
-            if os.path.join(dirname, filename).endswith('.jpeg') or os.path.join(dirname, filename).endswith('.jpg'):
-                dirname_li.append(dirname)
-                filenames_li.append(filenames)
-                total_path.append(os.path.join(dirname, filename))
-            else:
-                pass
-    
-    return dirname_li, filenames_li, total_path
+        dirname_li.append(dirname)
+        if '.DS_Store' in filenames:
+            pass
+        else:
+            filenames_li.append(filenames)
+       
+    return dirname_li, filenames_li
 
 
 if __name__ == "__main__":
-    dir_, file_, total = collectimages()
-    for i in range(len(dir_)):
-        threshold(dir_[i], file_[i], total[i])
+    dir_, file_ = collectimages()
+    #dir_.sort()
+    print(dir_.remove(dir_[0]))
+    print(file_)
+    for i in range(0, len(file_)):
+        threshold(dir_[i], file_[i])
+        
